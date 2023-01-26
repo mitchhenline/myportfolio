c0668668b1e11165350e86f9188d12402c471765
Mitch Henline <rmhenline@gmail.com>
2023-01-26 11:25:31 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,16 @@
+"""My portfolio server."""
+
+from flask import Flask, render_template
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key= "starship"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def homepage():
+    """view homepage"""
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
